c23fd0c913ea675a9aecf0a8123b6bf4cad0f43a
abara <alexibara.dev@gmail.com>
2022-07-11 14:48:05 +0200

    [fix] each pages has it's own css

--- src/View/index.php
+++ src/View/index.php
@@ -3,6 +3,7 @@
         <meta charset="utf-8">
         <link rel="stylesheet" href="<?php echo $this->asset('css/style.css') ?>">
         <link rel="stylesheet" href="<?php echo $this->asset('css/bootstrap.min.css') ?>">
+        <link rel="stylesheet" href="<?php echo $this->asset('css/pages/index.css') ?>">
     </head>
     <body>
         <div id="mmvc-panel-content">
